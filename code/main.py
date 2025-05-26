2bbf66f618be923857dfc933a72b5aca9562cdc1
ericonha <rheineric22@gmail.com>
2025-05-26 17:24:56 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -50,6 +50,16 @@ def get_german_month(english_month):
     }
     return months.get(english_month, "Invalid month")
 
+month_map = {
+    'January': 1, 'February': 2, 'March': 3, 'April': 4,
+    'May': 5, 'June': 6, 'July': 7, 'August': 8,
+    'September': 9, 'October': 10, 'November': 11, 'December': 12
+}
+
+# Só para ordenação: converte '3.1' → (3, 1), mas sem modificar o valor original
+def ap_id_sort_key(ap_id_str):
+    return tuple(map(int, ap_id_str.split('.')))
+
 
 def show_popup(message):
     msg = QMessageBox()
@@ -308,11 +318,29 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
 
     pre_define_workers = input_file.get_workers_pre_defined(df)
 
-    # AP.shuffle_aligned_lists(ap1.Nr, ids, lista_datas_not_to_change[0], lista_datas_not_to_change[1], pre_define_workers)
+    New_Nrs, New_ids, New_year_start, New_year_end, New_pre_define_workers, New_hours, Shuffle_to_Original_Index = AP.shuffle_aligned_lists(ap1.Nr, ids, lista_datas_not_to_change[0], lista_datas_not_to_change[1], pre_define_workers, ap1.hours)
 
-    h, ids_check, Nrs, pre_def = ap1.get_workers(lista_datas_not_to_change, ids, ap1.year_start, ap1.year_end, ap1.Nr,
+    h, ids_check, Nrs, pre_def = ap1.get_workers([New_year_start,New_year_end], New_ids, ap1.year_start, ap1.year_end, New_Nrs,
                                                  entity,
-                                                 df, pre_define_workers)
+                                                 df, New_pre_define_workers, New_hours)
+
+    #h, ids_check, Nrs, pre_def = ap1.get_workers(lista_datas_not_to_change, ids, ap1.year_start, ap1.year_end, ap1.Nr,
+    #                                             entity,
+    #                                             df, pre_define_workers, ap1.hours)
+
+    # For example, reconstruct data arrays
+    order_aps_final = sorted(AP.order_aps, key=lambda x: tuple(map(float, x[1].split("."))))
+
+    restored_Nrs = [x[0] for x in order_aps_final]
+    restored_ids = [x[1] for x in order_aps_final]
+    restored_start = [x[2] for x in order_aps_final]
+    restored_end = [x[3] for x in order_aps_final]
+    restored_dates = [x[4] for x in order_aps_final]
+    restored_hours = [x[5] for x in order_aps_final]
+    restored_workers = [x[6] for x in order_aps_final]
+
+
+
 
     html_content_1 = """
     <html lang="de">
@@ -388,38 +416,23 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
     years = np.linspace(ap1.year_start, ap1.year_end, ap1.year_end - ap1.year_start + 1)
     array_working_hours_per_year = np.zeros((len(worker.list_of_workers), len(years)))
 
-    for w, wh, dates_st, dates_ft, Nr, id, pre_w in zip(ap1.workers, h, ap1.working_dates_start,
-                                                        ap1.working_dates_end,
-                                                        Nrs, ids_check, pre_def):
-        if id in ids_check:
-            w_id = 0
-            if wh != 0:
-                w_id = w.id
-            row_color = "red" if w_id == 0 else "#ccffcc"
-
-            if w_id == 0:
-                sum_test += wh
-                ap_not_distribute.append(id)
-            else:
-                allocate_value(array_working_hours_per_year, dates_st, dates_ft, w_id, wh, years)
-                sum_test2 += wh
-            html_content_1 += f"""
-                        <tr style="background-color: {row_color};">
-                            <td>{id}</td>
-                            <td>{Nr}</td>
-                            <td>{dates_st}</td>
-                            <td>{dates_ft}</td>
-                            <td>{w_id}</td>
-                            <td>{wh}</td>
-                        </tr>
-                """
+    for w, wh, dates_st, dates_ft, Nr, id in zip(restored_workers,restored_hours,restored_start,restored_end,
+                                                 restored_Nrs,restored_ids):
+        html_content_1 += f"""
+                                <tr style="background-color: {"#ccffcc"};">
+                                    <td>{id}</td>
+                                    <td>{Nr}</td>
+                                    <td>{dates_st}</td>
+                                    <td>{dates_ft}</td>
+                                    <td>{w.id}</td>
+                                    <td>{round(wh,2)}</td>
+                                </tr>
+                        """
     html_content_1 += """
-                </table>
-                <div style="page-break-before: always;"></div>
-            </body>
-            </html>
-            """
-    print(sum_test)
+                    </table>
+                    <div style="page-break-before: always;"></div>
+                </body>
+                """
 
     # Generate HTML content with styling for the second table
     html_content_1 += """
@@ -468,7 +481,7 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
     new_array = []
     new_array_hours = []
     while len(worker.list_of_workers) != 0:
-        lowest_index_elem = worker.Worker(1000, 0, 0, 0, "", "")
+        lowest_index_elem = worker.Worker(1000, 0, 0, 0, "", "",0)
         for element in worker.list_of_workers:
             if element.id < lowest_index_elem.id:
                 lowest_index_elem = element
@@ -498,12 +511,22 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
     html_content_1 += "<td><strong>Total</strong></td>"
 
     workers_total_hours = []
+    index_year = 0
 
-    for workers_t in array_working_hours_per_year:
-        workers_total_hours.append(sum(workers_t))
+    for workers_t in worker.list_of_workers:
+        w_hours = []
+        for index_ele in range(len(workers_t.hours_available)):
+            w_hours.append(workers_t.hours_available[index_ele][0])
+        workers_total_hours.append(w_hours)
+
+    total_w = []
+    for index_ele in range(len(workers_total_hours)):
+        total_w.append(sum(np.array(hours_year_work_every_one[index_ele]) - np.array(workers_total_hours[index_ele])))
+
+
 
     # Add totals for each worker
-    for total in workers_total_hours:
+    for total in total_w:
         html_content_1 += f"<td><strong>{round(total,2)}</strong></td>"
 
     # Close the table and HTML body for the second table
@@ -536,7 +559,7 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
 
     html_content_1 += f"""
             <tr>
-                <td>{sum_t_b}</td>
+                <td>{round(sum_t_b,2)}</td>
                 <td>{sum_test}</td>
                 <td>{aps_str}</td>
                 <td>{cost_project_formatted}</td>
@@ -587,44 +610,64 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
                     </tr>
             """
 
+    #sorted_entries = sorted(
+    #    AP.global_data_zettel_infos.items(),  # Sort by worker_id
+    #    key=lambda x: (
+    #        x[0],  # Sort by worker_id (x[0] is the worker_id)
+    #        [entry['AP id'] for entry in x[1]],
+    #        [entry['year'] for entry in x[1]],  # Sort by year (x[1] contains the entries for each worker)
+    #        [entry['month'] for entry in x[1]]  # Sort by month (x[1] contains the entries for each worker)
+    #    )
+    #)
+
+    all_entries = [
+        entry
+        for worker_entries in AP.global_data_zettel_infos.values()
+        for entry in worker_entries
+    ]
+
     sorted_entries = sorted(
-        AP.global_data_zettel_infos.items(),  # Sort by worker_id
-        key=lambda x: (
-            x[0],  # Sort by worker_id (x[0] is the worker_id)
-            [entry['AP id'] for entry in x[1]],
-            [entry['year'] for entry in x[1]],  # Sort by year (x[1] contains the entries for each worker)
-            [entry['month'] for entry in x[1]]  # Sort by month (x[1] contains the entries for each worker)
+        all_entries,
+        key=lambda e: (
+            e['worker_id'],
+            ap_id_sort_key(e['AP id']),  # usa parse para ordenar, mas não altera o valor
+            month_map.get(e['month'], 0),  # mês como número
+            e['year'],
+            e['hours']
         )
     )
 
-    for worker_id, entries in sorted_entries:
-        for entry in entries:
-            # Extract month, hours, and PM
-            month = entry['month']
-            hours = entry['hours']
-            year = entry['year']
-            AP_id = entry['AP id']
 
-            if hours == 0:
-                continue
 
-            name = ""
 
-            for wks in worker.list_of_workers:
-                if wks.id == worker_id:
-                    name = str(wks.name) + " " + str(wks.surname)
+    for entry in sorted_entries:
+        # Extract month, hours, and PM
+        month = entry['month']
+        hours = entry['hours']
+        year = entry['year']
+        AP_id = entry['AP id']
+        worker_id = entry['worker_id']
 
-            # Add a row for each entry
-            html_content_2 += f"""
-            <tr>
-                <td>{name}</td>
-                <td>{AP_id}</td>
-                <td>{get_german_month(month)}</td>
-                <td>{year}</td>
-                <td>{hours * 160}</td>
-                <td>{hours}</td>
-            </tr>
-            """
+        if hours == 0:
+            continue
+
+        name = ""
+
+        for wks in worker.list_of_workers:
+            if wks.id == worker_id:
+                name = str(wks.name) + " " + str(wks.surname)
+
+        # Add a row for each entry
+        html_content_2 += f"""
+         <tr>
+            <td>{name}</td>
+            <td>{AP_id}</td>
+            <td>{get_german_month(month)}</td>
+            <td>{year}</td>
+            <td>{round(hours,2) * 160}</td>
+            <td>{round(hours,2)}</td>
+        </tr>
+        """
     html_content_2 += """
              </table>
          </body>
@@ -687,7 +730,7 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
                 html_content_2 += f"""
                 <tr>
                     <td>{str(wk.name) + " " + str(wk.surname)}</td>
-                    <td>{hours}</td>
+                    <td>{round(hours,2)}</td>
                     <td>{int(year)}</td>
                     <td>{months_german[month_idx]}</td>
                 </tr>
@@ -717,33 +760,27 @@ def run_process(df, filepath, filepath_workers, name_of_output_file, entity):
     pdf_output = BytesIO()
     pdf_output_2 = BytesIO()
 
-    HTML('output.html').write_pdf(name_of_output_file + "_datum" + ".pdf")
-    HTML('output2.html').write_pdf(name_of_output_file+"_organizer"+".pdf")
+    HTML('output.html').write_pdf(name_of_output_file + "_" + entity + "_datum" + ".pdf")
+    HTML('output2.html').write_pdf(name_of_output_file + "_" + entity + "_organizer"+".pdf")
     print("acabou")
 
 
 
-def round_0_25(duration):
-    duration = round_down_0_05(duration)
+def round_down_to_step(value, step):
+    return round((int(value / step)) * step, 10)
 
-    if round(duration * 4) / 4 != duration:
-        comparator = 0
-        while comparator <= duration:
-            comparator += 0.25
-        return comparator
-    return duration
+def round_up_to_step(value, step):
+    if value % step == 0:
+        return value
+    return ((int(value / step)) + 1) * step
 
-
-def round_down_0_05(number):
-    str_number = str(number)
-    decimal_part = str_number.split(".")[-1]
-
-    # Check if the number has more than one decimal place
-    if len(decimal_part) > 1:
-        return int(number * 20) / 20
-    else:
-        return number
-
+def round_to_step(value, step):
+    """
+    Rounds value up to the nearest multiple of `step`,
+    but first rounds down to nearest 0.05.
+    """
+    value = round_down_to_step(value, 0.05)  # optional pre-rounding
+    return round_up_to_step(value, step)
 
 def value_to_color(value):
     """Return a color based on the value. Near 0 is red, in the middle is blue, near 1 is green."""
